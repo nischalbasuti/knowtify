efc61064862486e6340deb5e1731d1bc3f500cf0
jaelse <ronald.smilingassassin@gmail.com>
2016-07-28 00:31:53 +0530

    commit 1

--- app/src/main/java/com/hydratech19gmail/notify/MainActivity.java
+++ app/src/main/java/com/hydratech19gmail/notify/MainActivity.java
@@ -3,6 +3,7 @@ package com.hydratech19gmail.notify;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("message","yo yo");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);

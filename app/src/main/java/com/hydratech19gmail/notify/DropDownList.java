8d45d8591d438243058806756ff9a961f6265806
Nischal Basuti <nischalbasuti1@gmail.com>
2016-08-22 23:21:45 +0530

    BroadcasterProfileActivity 1 viewholder in CustomAdapter back key in MainActivity takes us to home screen

---
+++ app/src/main/java/com/hydratech19gmail/notify/DropDownList.java
@@ -0,0 +1,7 @@
+package com.hydratech19gmail.notify;
+
+/**
+ * Created by nischal on 21/8/16.
+ */
+public class DropDownList {
+}
---
+++ app/src/main/java/com/hydratech19gmail/notify/NewBroadcastDialogue.java
@@ -0,0 +1,7 @@
+package com.hydratech19gmail.notify;
+
+/**
+ * Created by nischal on 18/8/16.
+ */
+public class NewBroadcastDialogue {
+}

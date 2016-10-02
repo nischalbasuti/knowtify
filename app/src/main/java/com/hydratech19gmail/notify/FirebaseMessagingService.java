8f01daa080147a34f537b45affe0812d23b34aeb
zappereton <ronald.smilingassassin@gmail.com>
2016-10-02 19:58:24 +0530

    Service notification from node js server

--- app/src/main/java/com/hydratech19gmail/notify/FirebaseMessagingService.java
+++ app/src/main/java/com/hydratech19gmail/notify/FirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,11 +16,16 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        showNotification(remoteMessage.getData().get("message"));
+        Log.d("remosteMessage",remoteMessage.getData().get("body").toString());
+        //String name = remoteMessage.getData().get("title");
+        //String subject = remoteMessage.getData().get("body");
+        //String content = remoteMessage.getData().get("Content");
+        //Log.d("title",name);
+        //showNotification(name);
         /*get the url for the new notification with the attachment url if any*/
     }
 
-    private void showNotification(String message) {
+    private void showNotification(String name) {
 
         Intent i = new Intent(this,MainActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -28,8 +34,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setAutoCancel(true)
-                .setContentTitle("Notify")
-                .setContentText(message)
+                .setContentTitle(name)
                 .setSmallIcon(R.drawable.ic_tab_broadcast)
                 .setContentIntent(pendingIntent);
 

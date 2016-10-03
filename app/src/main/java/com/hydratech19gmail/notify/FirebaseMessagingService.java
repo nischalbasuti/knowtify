dd79ae999b11a9775e4e4a980f5c1a0507ee9473
zappereton <ronald.smilingassassin@gmail.com>
2016-10-03 14:10:41 +0530

    Notification improvement(vibration,LED)

--- app/src/main/java/com/hydratech19gmail/notify/FirebaseMessagingService.java
+++ app/src/main/java/com/hydratech19gmail/notify/FirebaseMessagingService.java
@@ -3,6 +3,7 @@ package com.hydratech19gmail.notify;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -16,16 +17,16 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d("remosteMessage",remoteMessage.getData().get("body").toString());
-        //String name = remoteMessage.getData().get("title");
-        //String subject = remoteMessage.getData().get("body");
-        //String content = remoteMessage.getData().get("Content");
-        //Log.d("title",name);
-        //showNotification(name);
+
+        String name = remoteMessage.getData().get("title");
+        String subject = remoteMessage.getData().get("body");
+        String content = remoteMessage.getData().get("content");
+
+        showNotification(name,subject,content);
         /*get the url for the new notification with the attachment url if any*/
     }
 
-    private void showNotification(String name) {
+    private void showNotification(String name,String subject,String content) {
 
         Intent i = new Intent(this,MainActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -34,12 +35,16 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setAutoCancel(true)
+                .setVibrate(new long[]{250, 250, 250, 250, 250})
                 .setContentTitle(name)
+                .setContentText(subject)
+                .setLights(Color.RED,3000,3000)
                 .setSmallIcon(R.drawable.ic_tab_broadcast)
                 .setContentIntent(pendingIntent);
-
+            //need to add a sound for the notification
         NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
-        manager.notify(0,builder.build());
+        int num = (int) System.currentTimeMillis();
+        manager.notify(num,builder.build());
     }
 }

837b81738e10da1d8f1205b1c3be8985e456540b
zappereton <ronald.smilingassassin@gmail.com>
2016-10-13 20:55:35 +0530

    search fragment UI

--- app/src/main/java/com/hydratech19gmail/notify/SearchFragment.java
+++ app/src/main/java/com/hydratech19gmail/notify/SearchFragment.java
@@ -1,11 +1,22 @@
 package com.hydratech19gmail.notify;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Jaelse on 30-07-2016.
@@ -20,7 +31,24 @@ public class SearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search_fragment,container,false);
+
+        View rootView = inflater.inflate(R.layout.search_fragment,container,false);
+        SearchView searchView = (SearchView)rootView.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("Search",newText);
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+                return false;
+            }
+        });
+        return rootView;
 
     }
 }

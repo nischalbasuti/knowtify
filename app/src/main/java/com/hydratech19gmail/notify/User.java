e2455375884f0caa5643721e6c6ba68b9a46c33f
Nischal Basuti <nischalbasuti1@gmail.com>
2016-10-26 18:52:49 +0530

    *added photoUrl and emailId to User class

--- app/src/main/java/com/hydratech19gmail/notify/User.java
+++ app/src/main/java/com/hydratech19gmail/notify/User.java
@@ -1,18 +1,27 @@
 package com.hydratech19gmail.notify;
 
-/**
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/*
  * Created by nischal on 24/10/16.
  */
 
 public class User {
     private String uid;
     private String token;
+    private String emailid;
+    private String username;
+    private String photoUrl;
 
     public User(){}
 
-    public User(String uid, String token) {
+    public User(String uid, String token, String emailid, String username, String photoUrl) {
         this.uid = uid;
         this.token = token;
+        this.emailid = emailid;
+        this.username = username;
+        this.photoUrl = photoUrl;
     }
 
     //setters and getters
@@ -30,5 +39,29 @@ public class User {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 }

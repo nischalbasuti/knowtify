8ca1c86735da8c3faac81af36a69c658a0e885c8
jaelse <ronald.smilingassassin@gmail.com>
2016-12-06 01:06:17 +0530

    Log1

--- app/src/main/java/com/hydratech19gmail/notify/HomeFragment.java
+++ app/src/main/java/com/hydratech19gmail/notify/HomeFragment.java
@@ -72,7 +72,7 @@ public class HomeFragment extends Fragment{
                         Log.d("HomeFragment","Entered");
                         if(subscriptionKeyList.isEmpty() || !(subscriptionKeyList.isEmpty())){
                             subscriptionKeyList.addLast(subscriptionChannel.getKey());
-                            Log.d("HomeFragment","taking list again");
+                            Log.d("HomeFragment","taking list yoyoyo");
                             NotificationsListener notificationsListener = new NotificationsListener(listAdapter,getContext(),subscription.getSubscribersKey(),subscriptionChannel.getKey());
                             notificationsListener.getNotifications();
                         }
